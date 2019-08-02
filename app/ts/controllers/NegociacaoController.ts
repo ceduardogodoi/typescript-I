811563b46d3cb2086cf248cfb3b54cfed4561992
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-08-02 00:28:11 -0300

    Repetindo código

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -4,6 +4,7 @@ class NegociacaoController {
   private _inputValor: HTMLInputElement;
   private _negociacoes = new Negociacoes();
   private _negociacoesView = new NegociacoesView("#negociacoesView");
+  private _mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
     this._inputData = <HTMLInputElement>document.querySelector("#data");
@@ -12,7 +13,7 @@ class NegociacaoController {
     );
     this._inputValor = <HTMLInputElement>document.querySelector("#valor");
 
-    this._negociacoesView.update();
+    this._negociacoesView.update(this._negociacoes);
   }
 
   adiciona(event: Event) {
@@ -25,5 +26,8 @@ class NegociacaoController {
     );
 
     this._negociacoes.adiciona(negociacao);
+
+    this._negociacoesView.update(this._negociacoes);
+    this._mensagemView.update("Negociação adicionada com sucesso!");
   }
 }
