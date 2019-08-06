909f10faa0a26e1c993b6b25ed6da631c3c6891c
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-08-06 00:06:41 -0300

    Servidor local

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -1,7 +1,17 @@
-import { View } from "./View";
-export class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View_1, NegociacoesView;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
         <table class="table table-hover table-bordered">
           <thead>
             <tr>
@@ -14,17 +24,17 @@ export class NegociacoesView extends View {
 
           <tbody>
             ${model
-            .paraArray()
-            .map(negociacao => `
+                        .paraArray()
+                        .map(negociacao => `
                 <tr>
                   <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() +
-            1}/${negociacao.data.getFullYear()}</td>
+                        1}/${negociacao.data.getFullYear()}</td>
                   <td>${negociacao.quantidade}</td>
                   <td>${negociacao.valor}</td>
                   <td>${negociacao.volume}</td>
                 </tr>
               `)
-            .join("")}
+                        .join("")}
           </tbody>
 
           <tfoot>
@@ -32,5 +42,9 @@ export class NegociacoesView extends View {
           </tfoot>
         </table>
       `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
