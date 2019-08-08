39473d199f5479a95865e0067334e004f318a956
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-08-08 00:19:43 -0300

    Organizando módulos em barris

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,7 +1,5 @@
-import { Negociacoes } from "../models/Negociacoes";
-import { Negociacao } from "../models/Negociacao";
-import { NegociacoesView } from "../views/NegociacoesView";
-import { MensagemView } from "../views/MensagemView";
+import { NegociacoesView, MensagemView } from "../views/index";
+import { Negociacoes, Negociacao } from "../models/index";
 
 export class NegociacaoController {
   private _inputData: JQuery;
