b7ec036ef5e298b5889e1d697774b583f581589a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-09-12 00:56:25 -0300

    Evitando importar negociações duplicadas

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -67,10 +67,17 @@ export class NegociacaoController {
           throw new Error(res.statusText);
         }
       })
-      .then((negociacoes: Negociacao[]) => {
-        negociacoes.forEach(negociacao =>
-          this._negociacoes.adiciona(negociacao)
-        );
+      .then((negociacoesParaImportar: Negociacao[]) => {
+        const negociacoesJaImportadas = this._negociacoes.paraArray();
+
+        negociacoesParaImportar
+          .filter(
+            negociacao =>
+              !negociacoesJaImportadas.some(jaImportada =>
+                negociacao.ehIgual(jaImportada)
+              )
+          )
+          .forEach(negociacao => this._negociacoes.adiciona(negociacao));
 
         this._negociacoesView.update(this._negociacoes);
       });
